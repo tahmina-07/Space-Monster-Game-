742f5d708c6c48fddab22daea93835729990f16f
tahmina-07 <tahminasahar07@gmail.com>
2022-05-21 22:01:46 +0430

    adding some feature to the main file

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ pygame.init()
 #create a screen for our game
 screen = pygame.display.set_mode((800, 600))
 
+#creating the caption and logo of the game 
+pygame.display.set_caption("Space Monsters")
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_icon(icon)
+
+
+# Adding playes 
+playerIcon = pygame.image.load('alien.png')
+
+#main loop 
 #set a flag and we use while so the screen will last longer 
 running = True 
 while running:
@@ -13,6 +23,11 @@ while running:
         #if a quit button is pressed we will set the flag to false
         if event.type == pygame.QUIT:
             running = False
+    #add backround color to the main window
+    screen.fill((176, 38, 255))
+    pygame.display.update()
+
+
 
 
 
